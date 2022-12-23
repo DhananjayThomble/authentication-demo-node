fa8c1432e882012928304a7f03de0eda65502bec
dhananjaythomble <dhananjaythomble@gmail.com>
2022-12-23 19:39:59 +0530

    add level1 auth

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
-//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+
+// ------------------------------------db-------------------------------
+mongoose.set("strictQuery", "false");
+main().catch((err) => console.log(err));
+async function main() {
+  mongoose.connect(DB_URL);
+}
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+const User = new mongoose.model("user", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (err) console.error(err);
+    else {
+      console.log("user created!");
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username, password: password }, (err, result) => {
+    if (err) console.error(err);
+    else {
+      if(result){
+        res.render("secrets");
+      }
+      else{
+        res.send("Invalid credentials!");
+      }
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server started at " + PORT);
+});
